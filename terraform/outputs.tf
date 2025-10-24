98c3b29573250767720c11a48a6d34f61f4762f9
Ahmed M. Ibrahim El-Tahir <amieltahir@gmail.com>
2025-10-23 23:57:08 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,19 @@
-output "controller01_ip" { value = aws_instance.controller01.public_ip }
-output "hub01_ip"        { value = aws_instance.hub01.public_ip }
-output "exec01_ip"       { value = aws_instance.exec01.public_ip }
-output "db01_ip"         { value = aws_instance.db01.public_ip }
+output "controller01_ip" {
+  description = "Public IP of controller01"
+  value       = aws_instance.controller01.public_ip
+}
+
+output "hub01_ip" {
+  description = "Public IP of hub01"
+  value       = aws_instance.hub01.public_ip
+}
+
+output "exec01_ip" {
+  description = "Public IP of exec01"
+  value       = aws_instance.exec01.public_ip
+}
+
+output "db01_ip" {
+  description = "Public IP of db01"
+  value       = aws_instance.db01.public_ip
+}
