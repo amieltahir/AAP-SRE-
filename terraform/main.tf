44339cd223d94e9c01a186f429cf8397ea60cf0a
Ahmed M. Ibrahim El-Tahir <amieltahir@gmail.com>
2025-10-23 23:45:56 -0400

    Update CIDR block for SSH ingress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "aap_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["<YOUR_TRS_CORE_IP>/32"]
+    cidr_blocks = ["192.168.56.106/32"]
   }
 
   ingress {
