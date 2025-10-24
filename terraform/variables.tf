7f99a5a8d7095cc90fb968875a13a4fcefdf2268
Ahmed M. Ibrahim El-Tahir <amieltahir@gmail.com>
2025-10-23 23:37:00 -0400

    Update RHEL 9 AMI ID for aws_region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,2 @@
 variable "aws_region" { default = "us-east-1" }
-variable "ami_rhel9" { default = "ami-0c02fb55956c7d316" } # Replace with region-specific RHEL 9 AMI
+variable "ami_rhel9" { default = "ami-0dfc569a8686b9320" } # Replace with region-specific RHEL 9 AMI
